9db4e394897f4618edcde68725b1c0fce0746708
mosir <mosir@qq.com>
2026-06-24 23:49:39 +0800

    chore: update install.py for v2
    - Update OpenResty from 1.15.8.1 to 1.21.4.3
    - Add config.json bootstrap from config.default.json
    - Add config backup preservation on update
    - Add nginx user/group creation
    - Add backups/ directory creation
    - Add --with-stream --with-stream_ssl_module for additional proxy support
    - Fix update flow to save/restore runtime config

--- install.py
+++ install.py
@@ -1,103 +1,152 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2016-04-04 23:48
-# @Author  : Alexa (AlexaZhou@163.com)
-# @Link    : https://github.com/alexazhou/VeryNginx
-# @Disc    : install VeryNginx
-# @Disc    : support python 2.x and 3.x
+# @Date    : 2026-06-24
+# @Author  : VeryNginx v2
+# @Disc    : install VeryNginx v2 (support python 2.x and 3.x)
 
 import os
 import sys
 import getopt
 import filecmp
+import shutil
 
-#最新版openresty
+# ---------------------------------------------------------------------------
+# Configuration
+# ---------------------------------------------------------------------------
 
-openresty_pkg_url = 'https://openresty.org/download/openresty-1.15.8.1.tar.gz'
-openresty_pkg = 'openresty-1.15.8.1.tar.gz'
+openresty_pkg_url = 'https://openresty.org/download/openresty-1.21.4.3.tar.gz'
+openresty_pkg = 'openresty-1.21.4.3.tar.gz'
 
 work_path = os.getcwd()
 
-def install_openresty( ):
-    #check if the old version of VeryNginx installed( use upcase directory )
+# ---------------------------------------------------------------------------
+# Install OpenResty
+# ---------------------------------------------------------------------------
+
+def install_openresty():
     if os.path.exists('/opt/VeryNginx/VeryNginx') == True:
-        print("Seems that a old version of VeryNginx was installed in /opt/verynginx/...\nBefore install, please delete it and backup the configs if you need.")
+        print("Seems that a old version of VeryNginx was installed in /opt/verynginx/...")
+        print("Before install, please delete it and backup the configs if you need.")
         sys.exit(1)
-    
-    #makesure the dir is clean
+
     print('### makesure the work directory is clean')
     exec_sys_cmd('rm -rf ' + openresty_pkg.replace('.tar.gz',''))
-    
-    #download openresty
+
     down_flag = True
-    if os.path.exists( './' + openresty_pkg ):
+    if os.path.exists('./' + openresty_pkg):
         ans = ''
         while ans not in ['y','n']:
-            ans = common_input(' Found %s in current directory, use it?(y/n)'%openresty_pkg)
+            ans = common_input(' Found %s in current directory, use it?(y/n)' % openresty_pkg)
         if ans == 'y':
             down_flag = False
 
     if down_flag == True:
         print('### start download openresty package...')
         exec_sys_cmd('rm -rf ' + openresty_pkg)
-        exec_sys_cmd( 'wget ' + openresty_pkg_url )
+        exec_sys_cmd('wget ' + openresty_pkg_url)
     else:
         print('### use local openresty package...')
-    
+
     print('### release the package ...')
-    exec_sys_cmd( 'tar -xzf ' + openresty_pkg )
+    exec_sys_cmd('tar -xzf ' + openresty_pkg)
 
-    #configure && compile && install openresty
     print('### configure openresty ...')
-    os.chdir( openresty_pkg.replace('.tar.gz','') )
-    exec_sys_cmd( './configure --prefix=/opt/verynginx/openresty --user=nginx --group=nginx --with-http_v2_module --with-http_sub_module --with-http_stub_status_module --with-luajit' )
-    
+    os.chdir(openresty_pkg.replace('.tar.gz',''))
+    exec_sys_cmd(
+        './configure --prefix=/opt/verynginx/openresty '
+        '--user=nginx --group=nginx '
+        '--with-http_v2_module --with-http_sub_module '
+        '--with-http_stub_status_module --with-luajit '
+        '--with-stream --with-stream_ssl_module'
+    )
+
     print('### compile openresty ...')
-    exec_sys_cmd( 'make' )
-    
+    exec_sys_cmd('make')
+
     print('### install openresty ...')
-    exec_sys_cmd( 'make install' )
+    exec_sys_cmd('make install')
 
-    # print('### make nginx into PATH ...')
-    # May Not Work in CI
-    # exec_sys_cmd( 'export PATH=/opt/verynginx/openresty/nginx/sbin:$PATH' )
-
-    # print('### add user and group nginx:nginx')
-    # May Not Work in CI
-    # exec_sys_cmd( 'sudo groupadd -f nginx && useradd -g nginx nginx' )
+# ---------------------------------------------------------------------------
+# Install / Update VeryNginx
+# ---------------------------------------------------------------------------
 
 def install_verynginx():
-    
-    #install VeryNginx file
     print('### copy VeryNginx files ...')
-    os.chdir( work_path )
+    os.chdir(work_path)
+
     if os.path.exists('/opt/verynginx/') == False:
-        exec_sys_cmd( 'mkdir -p /opt/verynginx' )
-    
-    exec_sys_cmd( 'cp -r -f ./verynginx /opt/verynginx' )
+        exec_sys_cmd('mkdir -p /opt/verynginx')
 
-    #copy nginx config file to openresty
+    # Copy v2 source code (configs/config.json is gitignored, not overwritten)
+    exec_sys_cmd('cp -r -f ./verynginx /opt/verynginx')
+
+    # Bootstrap config: create config.json from template if not exists
+    config_json = '/opt/verynginx/verynginx/configs/config.json'
+    config_default = '/opt/verynginx/verynginx/configs/config.default.json'
+    if not os.path.exists(config_json):
+        if os.path.exists(config_default):
+            print('### bootstrap config: copy config.default.json -> config.json')
+            shutil.copyfile(config_default, config_json)
+            print('### IMPORTANT: edit config.json and set admin password_hash before use')
+        else:
+            print('### WARNING: no config.default.json found, will use built-in defaults')
+
+    # Ensure backups directory exists
+    backups_dir = '/opt/verynginx/verynginx/configs/backups'
+    if not os.path.exists(backups_dir):
+        os.makedirs(backups_dir)
+
+    # Copy nginx.conf to openresty (if openresty is installed and has default config)
+    openresty_conf = '/opt/verynginx/openresty/nginx/conf/nginx.conf'
+    openresty_conf_default = '/opt/verynginx/openresty/nginx/conf/nginx.conf.default'
     if os.path.exists('/opt/verynginx/openresty') == True:
-        if filecmp.cmp( '/opt/verynginx/openresty/nginx/conf/nginx.conf', '/opt/verynginx/openresty/nginx/conf/nginx.conf.default', False ) == True:
-            print('cp nginx config file to openresty')
-            exec_sys_cmd( 'cp -f ./nginx.conf  /opt/verynginx/openresty/nginx/conf/' )
+        if os.path.exists(openresty_conf_default) and filecmp.cmp(openresty_conf, openresty_conf_default, False) == True:
+            print('### copy nginx config file to openresty')
+            exec_sys_cmd('cp -f ./nginx.conf /opt/verynginx/openresty/nginx/conf/')
+        elif not os.path.exists(openresty_conf_default):
+            print('### copy nginx config file to openresty (first install)')
+            exec_sys_cmd('cp -f ./nginx.conf /opt/verynginx/openresty/nginx/conf/')
     else:
-        print( 'openresty not fount, so not copy nginx.conf' )
+        print('### openresty not found, so not copying nginx.conf')
 
-    # set mask for the path which used for save configs
-    # file too open;
-    # exec_sys_cmd( 'chmod -R 777 /opt/verynginx/verynginx/configs' )
-    exec_sys_cmd( 'chmod -R 755 /opt/verynginx/verynginx/configs' )
+    # Set permissions for config storage
+    exec_sys_cmd('chmod -R 755 /opt/verynginx/verynginx/configs')
 
+    print('### create nginx user/group if not exist')
+    exec_sys_cmd('id -u nginx > /dev/null 2>&1 || useradd -r -s /sbin/nologin nginx', accept_failed=True)
 
 def update_verynginx():
-    install_verynginx()    
+    print('### WARNING: update will keep existing config.json')
+    print('### Backup your config:\n    cp /opt/verynginx/verynginx/configs/config.json ~/')
+    ans = common_input('Continue? (y/n): ')
+    if ans != 'y':
+        print('Update cancelled.')
+        return
 
+    # Backup config
+    config_json = '/opt/verynginx/verynginx/configs/config.json'
+    config_backup = '/opt/verynginx/verynginx/configs/config.json.update_backup'
+    if os.path.exists(config_json):
+        shutil.copyfile(config_json, config_backup)
+        print('### backup saved to ' + config_backup)
 
-def exec_sys_cmd(cmd, accept_failed = False):
-    print( cmd )
-    ret = os.system( cmd )
-    if  ret == 0:
+    install_verynginx()
+
+    # Restore config
+    if os.path.exists(config_backup):
+        shutil.copyfile(config_backup, config_json)
+        print('### config restored from backup')
+
+    print('### update complete')
+
+# ---------------------------------------------------------------------------
+# Helpers
+# ---------------------------------------------------------------------------
+
+def exec_sys_cmd(cmd, accept_failed=False):
+    print(cmd)
+    ret = os.system(cmd)
+    if ret == 0:
         return ret
     else:
         if accept_failed == False:
@@ -106,11 +155,11 @@ def exec_sys_cmd(cmd, accept_failed = False):
         else:
             return False
 
-def common_input( s ):
+def common_input(s):
     if sys.version_info[0] == 3:
-        return input( s )
+        return input(s)
     else:
-        return raw_input( s )
+        return raw_input(s)
 
 def safe_pop(l):
     if len(l) == 0:
@@ -119,27 +168,30 @@ def safe_pop(l):
         return l.pop(0)
 
 def show_help_and_exit():
-    help_doc = 'usage: install.py <cmd> <args> ... \n\n\
-install cmds and args:\n\
-    install\n\
-        all        :  install verynginx and openresty(default)\n\
-        openresty  :  install openresty\n\
-        verynginx  :  install verynginx\n\
-    update\n\
-        verynginx  :  update the installed verynginx\n\
-    '
+    help_doc = '''usage: install.py <cmd> <args> ...
+
+install cmds and args:
+    install
+        all        :  install verynginx and openresty (default)
+        openresty  :  install openresty only
+        verynginx  :  install verynginx only
+    update
+        verynginx  :  update the installed verynginx
+    '''
     print(help_doc)
     exit()
 
+# ---------------------------------------------------------------------------
+# Entry
+# ---------------------------------------------------------------------------
 
 if __name__ == '__main__':
+    opts, args = getopt.getopt(sys.argv[1:], '', [])
 
-    opts, args = getopt.getopt(sys.argv[1:], '', []) 
-  
     cmd = safe_pop(args)
     if cmd == 'install':
         cmd = safe_pop(args)
-        if cmd == 'all' or cmd == None:
+        if cmd == 'all' or cmd is None:
             install_openresty()
             install_verynginx()
         elif cmd == 'openresty':
@@ -159,8 +211,7 @@ if __name__ == '__main__':
 
     print('*** All work finished successfully, enjoy it~')
 
-
 else:
-    print ('install.py had been imported as a module')
-    print ('please add group and user nginx:nginx')
-    print ('to use nginx, add it in PATH: \nexport PATH=/opt/verynginx/openresty/nginx/sbin:$PATH')
+    print('install.py loaded as module')
+    print('To use nginx, add it in PATH:')
+    print('export PATH=/opt/verynginx/openresty/nginx/sbin:$PATH')
